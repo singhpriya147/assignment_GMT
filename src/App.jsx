23a8c4edeadfbc9c691ac88c98d3fc7f77d898e0
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2024-07-09 22:17:15 +0530

    added login screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 ;
 import Registration from './pages/Registration';
 import Login from './pages/Login';
+
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Registration/>}/>
+      
+        <Route path='/registration' element={<Registration/>}/>
         <Route path='/login' element={<Login/>}/>
       </Routes>
     </Router>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,36 @@
 
 
-const Login = () => {
+import '../styles/Login.css';
+import GoogleIcon from '../assets/icons/icons8-google.svg?react';
+const Login= () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className='login-wrapper'>
+      <div className='login-header'>
+        <h2>Login to your acccount</h2>
+        <p>Please sign in to your account</p>
+        <form action=''>
+          <label htmlFor='email'>Email Address</label>
+          <input id='email' type='email' placeholder='johndoe@gmail.com' />
+          <label htmlFor='username'>User Name</label>
+          <input type='text' id='username' />
+          <label htmlFor='password'>Password</label>
+          
+          <button>Register</button>
+        </form>
+        <div className='login-footer'>
+          <div className='hr-lines'>
+            <span>Or sign in with</span>
+          </div>
+          <div className='footer-base'>
+            <GoogleIcon width='25' height='25' />
+            <p>
+              Have an Account ? <a>Sign In</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
