99e7e35352ed52615026b5b5ce4ba430580aefc6
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2024-07-09 21:28:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+;
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Registration/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
+
+  )
+}
+
+export default App
