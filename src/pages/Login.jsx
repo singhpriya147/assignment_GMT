97780ecaf7e5ff054d0047846f9f83a9b46754c7
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2024-07-11 21:03:10 +0530

    added firebase authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,20 +2,60 @@
 
 import '../styles/Login.css';
 import GoogleIcon from '../assets/icons/icons8-google.svg?react';
+
+import { useState } from 'react';
+import { doSignInWithEmailAndPassword } from '../firebase/auth';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+
+
 const Login= () => {
+  const navigate = useNavigate();
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+const onSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    await doSignInWithEmailAndPassword(email, password);
+    console.log("user logged in ")
+    navigate('/postlogin');
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+   
+    setEmail('');
+    setPassword('');
+  }
+};
+
+
+   
   return (
     <div className='login-wrapper'>
       <div className='login-header'>
         <h2>Login to your acccount</h2>
         <p>Please sign in to your account</p>
-        <form action=''>
+        <form onSubmit={onSubmit}>
           <label htmlFor='email'>Email Address</label>
-          <input id='email' type='email' placeholder='johndoe@gmail.com' />
-          <label htmlFor='username'>User Name</label>
-          <input type='text' id='username' />
+          <input
+            id='email'
+            type='email'
+            placeholder='johndoe@gmail.com'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
           <label htmlFor='password'>Password</label>
-          
-          <button>Register</button>
+          <input
+            id='password'
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type='submit'>Login</button>
         </form>
         <div className='login-footer'>
           <div className='hr-lines'>
@@ -24,7 +64,7 @@ const Login= () => {
           <div className='footer-base'>
             <GoogleIcon width='25' height='25' />
             <p>
-              Have an Account ? <a>Sign In</a>
+              Don't have an Account? <Link to='/registration'>Register</Link>
             </p>
           </div>
         </div>
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,39 +1,119 @@
 import '../styles/Registration.css'
 import  GoogleIcon from '../assets/icons/icons8-google.svg?react';
+
+// import { useAuth } from '../context/authContext';
+import { doCreateUserWithEmailAndPassword } from '../firebase/auth';
+import {doSignInWithGoogle} from '../firebase/auth'
+import { useState } from 'react';
+import { Link} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+// import {toast} from 'react-toastify'
+
+
 const Registration = () => {
+
+  const navigate = useNavigate();
+const[email,setEmail]=useState('');
+const[password,setPassword]=useState('');
+const[username,setUsername]=useState('');
+// const[isRegistering,setIsRegistering]=useState(false);
+// const[errorMessage,setErrorMessage]=useState('')
+
+// const{userLoggedIn}=useAuth()
+// const navigate = useNavigate();
+const onSubmit=async(e)=>{
+e.preventDefault();
+
+
+try {
+  await doCreateUserWithEmailAndPassword(email, password);
+  navigate('/login');
+} catch (error) {
+ 
+  console.log(error.message);
+} finally {
+  
+  setUsername('');
+  setEmail('');
+  setPassword('');
+}
+
+
+}
+
+
+const registerWithGoogle=async()=>{
+try {
+  await doSignInWithGoogle (email, password);
+  navigate('/postlogin');
+} catch (error) {
+   console.log(error.message);
+}
+}
+
+
   return (
-    <div className='registration-wrapper'>
-      <div className='regis-header'>
-        <h2>Create your New acccount</h2>
-        <p>Create an account to start looking for the food you like</p>
-        <form action=''>
-          <label htmlFor='email'>Email Address</label>
-          <input id='email' type='email' placeholder='johndoe@gmail.com' />
-          <label htmlFor='username'>User Name</label>
-          <input type='text' id='username' />
-          <label htmlFor='password'>Password</label>
-          <input type='password' name='' id='password' />
-          <div className='Term-condition'>
-            <input type='checkbox' name='' id='check' />
-            <p>
-              I Agree with <a>Term of services</a>and <a>Privacy policy</a>
-            </p>
-          </div>
-          <button>Register</button>
-        </form>
-        <div className='registration-footer'>
-          <div className='hr-lines'>
-            <span>Or sign in with</span>
-          </div>
-          <div className='footer-base'>
-            <GoogleIcon width='25' height='25' />
-            <p>
-              Have an Account ? <a>Sign In</a>
-            </p>
+    <>
+      {/* {userLoggedIn && <Navigate to={'/postlogin'} replace={true} />} */}
+
+      <div className='registration-wrapper'>
+        <div className='regis-header'>
+          <h2>Create your New acccount</h2>
+          <p>Create an account to start looking for the food you like</p>
+          <form onSubmit={onSubmit}>
+            <label htmlFor='email'>Email Address</label>
+            <input
+              id='email'
+              type='email'
+              placeholder='johndoe@gmail.com'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor='username'>User Name</label>
+            <input
+              type='text'
+              id='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name=''
+              id='password'
+              // disabled={isRegistering}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className='Term-condition'>
+              <input type='checkbox' name='' id='check' />
+              <p>
+                I Agree with <a>Term of services</a>and <a>Privacy policy</a>
+              </p>
+            </div>
+
+            {/* {errorMessage && (
+              <span className='text-red-600 font-bold'>{errorMessage}</span>
+            )} */}
+
+            <button type='subit' >
+              Register
+            </button>
+          </form>
+          <div className='registration-footer'>
+            <div className='hr-lines'>
+              <span>Or sign in with</span>
+            </div>
+            <div className='footer-base'>
+              <GoogleIcon width='25' height='25'   onClick={registerWithGoogle}/>
+              <p>
+                Have an Account ? <Link to={'/login'}>Sign In</Link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
